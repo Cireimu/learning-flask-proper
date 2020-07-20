5622a7e2995df76a534333809238d95c6670c8ae
Cireimu <ethanhoover3@gmail.com>
2020-07-20 15:10:54 -0700

    basic settup

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def hello():
+    return {"hello": "world"}
+
+
+class CarsModel(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    model = db.Column(db.String())
+    doors = db.Column(db.Integer())
+
+    def __init__(self, name, model, doors):
+        self.name = name
+        self.model = model
+        self.doors = doors
+
+    def __repr__(self):
+        return f"<Car {self.name}>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
